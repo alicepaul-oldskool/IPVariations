4234870e14080c409767d73bbafe24c4e003a6a8
Leslie A. Gruen <leslie.gruen@gmail.com>
2012-10-29 00:59:15 -0400

    Random Directory and file script :)

---
+++ lib_scripts_ref/file_utilites.php
@@ -0,0 +1,32 @@
+<?php
+if ($handle = opendir('/dev-survey/images_live')) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "$entry\n";
+        }
+    }
+    closedir($handle);
+}
+?>
+
+
+<?php
+/*
+if ($handle = opendir('/dev-survey/images_live')) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry\n";
+    }
+
+    /* This is the WRONG way to loop over the directory. */
+    while ($entry = readdir($handle)) {
+        echo "$entry\n";
+    }
+
+    closedir($handle);
+}
+?>*/
+<a href="../dev-survey/images_live">images_live</a>
